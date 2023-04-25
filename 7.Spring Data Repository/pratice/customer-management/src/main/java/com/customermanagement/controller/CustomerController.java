243c83aaaab6042ccbd2e108cd75ade3f168e4ef
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-04-25 18:39:29 +0700

    0.

---
+++ 7.Spring Data Repository/pratice/customer-management/src/main/java/com/customermanagement/controller/CustomerController.java
@@ -0,0 +1,32 @@
+package com.customermanagement.controller;
+
+import com.customermanagement.model.Customer;
+import com.customermanagement.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CustomerController {
+
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping("/create-customer")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-customer")
+    public ModelAndView saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("/customer/create");
+        modelAndView.addObject("customer", new Customer());
+        modelAndView.addObject("message", "create successfully");
+        return modelAndView;
+    }
+
+}
---
+++ 7.Spring Data Repository/pratice/customer-management/src/main/java/com/customermanagement/service/CustomerService.java
@@ -0,0 +1,32 @@
+package com.customermanagement.service;
+
+import com.customermanagement.model.Customer;
+import com.customermanagement.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class CustomerService implements ICustomerService{
+@Autowired
+private ICustomerRepository customerRepository;
+    @Override
+    public Iterable<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Customer> findById(Integer id) {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Integer id) {
+        customerRepository.deleteById(id);
+    }
+}
