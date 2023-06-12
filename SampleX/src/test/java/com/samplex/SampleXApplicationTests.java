36d999c5043042cae67621fa7723f95d285ff229
PhamHoPhucBinh <108465513+PhamHoPhucBinh@users.noreply.github.com>
2023-06-12 15:47:18 +0700

    done

---
+++ SampleX/src/test/java/com/samplex/SampleXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
